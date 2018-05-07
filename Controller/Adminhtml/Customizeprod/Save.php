1c9c411c3f115a86070b55cf504b869aac25013f
Sunil Verma <sunil.gcet@gmail.com>
2018-05-07 17:00:44 +0530

    Category id issue resolved

--- Controller/Adminhtml/Customizeprod/Save.php
+++ Controller/Adminhtml/Customizeprod/Save.php
@@ -11,14 +11,18 @@ class Save extends \Magento\Backend\App\Action
 		
         $data = $this->getRequest()->getParams();
         if ($data) {
+			
+			$categorySelected = array();
+		
             $model = $this->_objectManager->create('Chilliapple\Customizeproducts\Model\Customizeprod');
 			$id = $this->getRequest()->getParam('id');
             if ($id) {
                 $model->load($id);
             }
-			$categorySelected = $data['category_id'];
-			$data['category_id'] = implode(",", $data['category_id']);
-			
+			if(isset($data['category_id']) && count($data['category_id'])) {
+				$categorySelected = $data['category_id'];
+				$data['category_id'] = implode(",", $data['category_id']);
+			}
 			//echo "<pre>"; print_r($data); exit;
 			
             $model->setData($data);
@@ -37,10 +41,12 @@ class Save extends \Magento\Backend\App\Action
 				$sql = "DELETE FROM " . $tableName." Where product_id = '".$data['id']."'";
 				$connection->query($sql);
 				
-				//Insert Data into table
-				foreach($categorySelected as $catId) {
-					$sql = "INSERT INTO " . $tableName . " (product_id, category_id) Values ('".$data['id']."','".$catId."')";
-					$connection->query($sql);
+				if(isset($categorySelected) && count($categorySelected) > 0) {
+					//Insert Data into table
+					foreach($categorySelected as $catId) {
+						$sql = "INSERT INTO " . $tableName . " (product_id, category_id) Values ('".$data['id']."','".$catId."')";
+						$connection->query($sql);
+					}
 				}
                 $this->messageManager->addSuccess(__('The customize product has been saved.'));
                 $this->_objectManager->get('Magento\Backend\Model\Session')->setFormData(false);
@@ -55,7 +61,7 @@ class Save extends \Magento\Backend\App\Action
             } catch (\RuntimeException $e) {
                 $this->messageManager->addError($e->getMessage());
             } catch (\Exception $e) {
-                $this->messageManager->addException($e, __('Something went wrong while saving the banner.'));
+                $this->messageManager->addException($e, __('Something went wrong while saving the product.'));
             }
 
             $this->_getSession()->setFormData($data);

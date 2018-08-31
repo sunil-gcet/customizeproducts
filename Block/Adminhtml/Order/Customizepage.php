66a0108a28602fe8aaf9a243a0b691700c0de5d4
Sunil Verma <sunil.gcet@gmail.com>
2018-08-31 15:58:01 +0530

    Update Customizepage.php
    Applied condition for order detail page so the designer load only if any product is customized else not.

--- Block/Adminhtml/Order/Customizepage.php
+++ Block/Adminhtml/Order/Customizepage.php
@@ -1,6 +1,6 @@
 <?php
 namespace Chilliapple\Customizeproducts\Block\Adminhtml\Order;
-
+use Chilliapple\Customizeproducts\Model\Customizecartdata;
 class Customizepage extends \Magento\Sales\Block\Adminhtml\Order\View
 {
 	public function __construct(
@@ -10,18 +10,19 @@ class Customizepage extends \Magento\Sales\Block\Adminhtml\Order\View
         \Magento\Sales\Helper\Reorder $reorderHelper,
 		\Chilliapple\Customizeproducts\Helper\Data $helperData,
 		\Chilliapple\Customizeproducts\Helper\Ajaxfunctions $ajaxhelperData,
+		Customizecartdata $customizeCartdataRes,
         array $data = []
     ) {
 		$this->_helperData = $helperData;
 		$this->_ajaxhelperData = $ajaxhelperData;
+		$this->_customizeCartdataRsrce = $customizeCartdataRes;
         parent::__construct($context, $registry, $salesConfig, $reorderHelper, $data);
     }
 	
 	public function getenabledFonts() {
 		$google_webfonts = $this->_helperData->getEnabledGoogleFonts();
 		$max_fonts_per_href = 10;
-		$href_wf = array();
-		$href_wf_text = '';
+		$href_wf = array();		$href_wf_text = '';
 		if (!empty($google_webfonts)) {
 			for ($i=0; $i < sizeof($google_webfonts); $i++) {
 				array_push($href_wf, $google_webfonts[$i]);
@@ -31,5 +32,25 @@ class Customizepage extends \Magento\Sales\Block\Adminhtml\Order\View
 			}
 		}
 		return $href_wf_text;
+	}
+	
+	public function ifCustomizeProductExists() {
+		$havecustomizedProducts = false;
+		$items = $this->getOrder()->getAllItems();
+		foreach ($items as $item) {
+			if($this->getProductCustomization($item->getProductId())) {
+				$havecustomizedProducts = true;
+			}
+		}
+		return $havecustomizedProducts;
+	}
+	
+	private function getProductCustomization($productId) {
+		$orderincId = $this->getOrder()->getIncrementId();
+		$productData = $this->_customizeCartdataRsrce->getOrderedproductData($orderincId, $productId);
+		if(count($productData) > 0) {
+			return true;
+		}
+		return false;
 	}
 }
